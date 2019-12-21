13dd37d9d8fc3501aba0996ad35c5e9f3e0175e0
MadCat-777 <MadCat-777@yandex.ru>
2019-12-21 09:11:45 +0500

    lab_0003

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 //==========
 #include <iostream>
+#include <string>
 //==========
 using namespace std;
 //==========
 int main()
 {
    cout<<"Cake is lie"<<endl;
+   
+   string inpt;
+   cin>>inpt;
+   
    return 0;
 }
 //==========
