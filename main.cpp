23e35bf0cfbeebfde446e6980af4de1bfc130805
MadCat-777 <MadCat-777@yandex.ru>
2019-12-21 09:27:47 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,7 +10,68 @@ int main()
    
    string inpt;
    cin>>inpt;
-   cout<<inpt<<endl;
+   
+   int temp=0, num=0;
+   
+   num=size(inpt);
+   
+   for (int i = 0; i < num; i++)
+   {
+      switch (inpt[i])
+      {
+         case '0':
+            temp++;
+         break;
+
+         case '1':
+            temp++;
+         break;
+
+         case '2':
+            temp++;
+         break;
+
+         case '3':
+            temp++;
+         break;
+
+         case '4':
+            temp++;
+         break;
+
+         case '5':
+            temp++;
+         break;
+
+         case '6':
+            temp++;
+         break;
+
+         case '7':
+            temp++;
+         break;
+
+         case '8':
+            temp++;
+         break;
+
+         case '9':
+            temp++;
+         break;
+
+         default:
+         break;
+      }
+   }
+
+   if(temp == num)
+   {
+      cout << true << endl;
+   }
+   else
+   {
+      cout << false << endl;
+   }
    
    return 0;
 }
